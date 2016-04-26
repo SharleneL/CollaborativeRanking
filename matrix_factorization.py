536ffb36cce69620c5a182ec6beea9f9c3bf2bde
Shalin Luo <luoshalin@LuoShalins-MacBook-Pro.local>
2016-04-26 01:20:14 -0400

    pmf code cleaned

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -1,17 +1,6 @@
 __author__ = 'luoshalin'
 
-from scipy import sparse
-import os
-import sys
 import numpy as np
-from sklearn.metrics import pairwise_distances
-from scipy.spatial.distance import cosine
-from scipy import spatial
-import heapq
-from numpy import linalg as LAn
-import time
-
-from scipy.sparse import *
 from scipy import *
 
 
@@ -50,25 +39,11 @@ def pmf(R):
             break
         last_error = cal_error(R, U, V, I)
 
-        print "Iteration:%d,E:%d"%(i, last_error)
+        print "Iteration #" + str(i) + "; Err: " + str(last_error)
     # ASSERT: GOT U & V
-    # print self._U.shape
-    # print self._V.shape
 
 
 def cal_error(R, U, V, I):
-    # print 'error:'
-    # print R.shape
-    # print np.dot(U, V.T).shape
-    # print I.shape
-    tmpM1 = np.dot(U, V.T)
-    tmpM2 = R - tmpM1
-    # I = sparse.csr_matrix(I)
-    # print type(I)
-    # print type(tmpM2)
-    # tmpM3 = I * tmpM2
-    tmpM3 = I * np.asarray(tmpM2, dtype=np.float64)
-    tmpM4 = tmpM3 ** 2
-    error = np.sum(tmpM4)
+    error = np.sum((I * np.asarray((R - np.dot(U, V.T)), dtype=np.float64)) ** 2)
     # error = np.sum((I * (R - np.dot(U, V.T))) ** 2)
     return error
